2274be2a892a456185e811db5e0d09d3e9a0b948
Ark Studio <arkstudio.com.co@gmail.com>
2026-05-26 22:49:23 -0500

    feat: integrate Google Tag Manager (GTM-N94BMTTF)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Cinzel, Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/footer";
@@ -89,6 +90,12 @@ export default function RootLayout({
         >
           Saltar al contenido principal
         </a>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N94BMTTF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -125,6 +132,17 @@ export default function RootLayout({
         <WhatsAppButton />
         <Footer />
       </body>
+      <Script
+        id="gtm-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N94BMTTF');`,
+        }}
+      />
     </html>
   );
 }
